8a201b4559e518f22f4ff24c31fae1d388cc278a
firsovpro <firsovpro@yandex.ru>
2023-11-12 14:39:26 +0300

    auch mysql

--- webclient/server/config/db.config.js
+++ webclient/server/config/db.config.js
@@ -3,17 +3,6 @@ module.exports = {
     YANDEX_CLIENT_SECRET: 'bed01bb32c814c65891719ec006c46ed',
     callbackURL: 'https://firsovpro.online:3001/auth/yandex/callback',
     cookieSecret: '244556677',
-    HOST: "rent21.ru",
-    USER: "joomsql",
-    PASSWORD: "5895472",
-    DB: "node",
-    dialect: "mysql",
-    postgress: {
-        user: 'rent21',
-        password: 'gst42WEB',
-        db: 'node',
-        host: 'rent21.ru'
-    },
     operatorsAliases: 0,
     pool: {
         max: 5,
